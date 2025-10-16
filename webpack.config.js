4994ca0155843ada0943856fe08b56b5e37f37ff
marascott <david.asquiedge@marascott.ai>
2025-10-16 15:45:12 +0000

    Add index template

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
+import fs from 'node:fs'
+import path from 'node:path'
 import { createExpoWebpackConfigAsync } from '@expo/webpack-config'
 
 export default async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv)
 
   config.output.publicPath = './';
+
+  const templatePath = path.resolve(__dirname, 'public/index.html')
+
+  if (fs.existsSync(templatePath) && Array.isArray(config.plugins)) {
+    config.plugins.forEach((plugin) => {
+      if (plugin?.constructor?.name === 'HtmlWebpackPlugin') {
+        if (plugin.userOptions) {
+          plugin.userOptions.template = templatePath
+        }
+        if (plugin.options) {
+          plugin.options.template = templatePath
+        }
+      }
+    })
+  }
 
   // ✅ Enable polling (good for WSL2, Docker, or network drives)
   config.watchOptions = {
